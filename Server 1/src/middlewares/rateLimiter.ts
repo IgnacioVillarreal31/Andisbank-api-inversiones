48bceeab67abbc0677e9ebf0c3145cb1b6606976
IgnacioVillarreal31 <ignacio.villarreal@correo.ucu.edu.uy>
2024-09-25 20:56:10 -0300

    u

--- Server 1/src/middlewares/rateLimiter.ts
+++ Server 1/src/middlewares/rateLimiter.ts
@@ -1,17 +1,59 @@
 import rateLimit from 'express-rate-limit';
+const { RateLimiterMemory } = require("rate-limiter-flexible");
 
 export const rateLimiterFixedWindow = rateLimit({
     windowMs: 24 * 60 * 60 * 1000,
-    max: 100,
-    message: 'You have exceeded the 100 requests in 24 hrs limit!', 
+    max: 999,
+    message: 'You have exceeded the 100 requests in 24 hrs limit!',
     standardHeaders: true,
     legacyHeaders: false,
 });
 
-export const rateLimiterSlidingWindow = rateLimit({
-    windowMs: 24 * 60 * 60 * 1000,
-    max: 100,
-    message: 'You have exceeded the 100 requests in 24 hrs limit!', 
-    standardHeaders: true,
-    legacyHeaders: false,
+const slidingWindow = new RateLimiterMemory({
+    points: 200,
+    duration: 24 * 60 * 60,
+    execEvenly: true,
+    keyPrefix: 'middleware',
 });
+
+export const rateLimiterSlidingWindow = (req: any, res: any, next: any) => {
+    slidingWindow.consume(req.ip)
+        .then(() => {
+            next();
+        })
+        .catch(() => {
+            res.status(429).send('Too Many Requests');
+        });
+};
+
+const concurrency = new RateLimiterMemory({
+    points: 10,
+    duration: 1,
+    blockDuration: 10,
+});
+
+export const rateLimiterConcurrency = (req: any, res: any, next: any) => {
+    concurrency.consume(req.ip)
+        .then(() => {
+            next();
+        })
+        .catch(() => {
+            res.status(429).send('Too many concurrent requests!');
+        });
+};
+
+const tokenBucket = new RateLimiterMemory({
+    points: 100,
+    duration: 60,
+    blockDuration: 5,
+});
+
+export const rateLimiterTokenBucket = (req: any, res: any, next:any) => {
+    tokenBucket.consume(req.ip)
+        .then(() => {
+            next();
+        })
+        .catch(() => {
+            res.status(429).send('Too many requests, please slow down.');
+        });
+};
--- Server 1/src/routes/investmentRoutes.ts
+++ Server 1/src/routes/investmentRoutes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { getInvestments, getInvestment, postInvestmentAdmin, postBuyInvestmentUser, postSellInvestmentUser, deleteInvestmentAdmin, putInvestmentAdmin, getUserInvestment} from '../controllers/investmentController';
-import { rateLimiterFixedWindow } from '../middlewares/rateLimiter';
+import { rateLimiterFixedWindow, rateLimiterSlidingWindow, rateLimiterConcurrency, rateLimiterTokenBucket } from '../middlewares/rateLimiter';
 
 const router: Router = Router();
 
 router.get('/getInvestments', rateLimiterFixedWindow, getInvestments);
 router.get('/getInvestment/:idInvestment', rateLimiterFixedWindow, getInvestment);
 router.post('/postInvestmentAdmin', rateLimiterFixedWindow, postInvestmentAdmin);
-router.post('/postBuyInvestmentUser', postBuyInvestmentUser);
-router.post('/postSellInvestmentUser', postSellInvestmentUser);
-router.delete('/deleteInvestmentAdmin/:idInvestment', deleteInvestmentAdmin);
-router.put('/putInvestmentAdmin/:idInvestment', putInvestmentAdmin);
-router.get('/getUserInvestment/:username', getUserInvestment);
+router.post('/postBuyInvestmentUser', rateLimiterSlidingWindow, postBuyInvestmentUser);
+router.post('/postSellInvestmentUser', rateLimiterSlidingWindow, postSellInvestmentUser);
+router.delete('/deleteInvestmentAdmin/:idInvestment', rateLimiterConcurrency, deleteInvestmentAdmin);
+router.put('/putInvestmentAdmin/:idInvestment', rateLimiterConcurrency, putInvestmentAdmin);
+router.get('/getUserInvestment/:username', rateLimiterTokenBucket, getUserInvestment);
 
 export default router;
