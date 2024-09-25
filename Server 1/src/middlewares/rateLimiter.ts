961cc24a03e34f71d0f928e26450a4670a09257b
IgnacioVillarreal31 <ignacio.villarreal@correo.ucu.edu.uy>
2024-09-25 19:53:12 -0300

    a

--- Server 1/src/middlewares/rateLimiter.ts
+++ Server 1/src/middlewares/rateLimiter.ts
@@ -2,7 +2,15 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimiterFixedWindow = rateLimit({
     windowMs: 24 * 60 * 60 * 1000,
-    max: 2,
+    max: 100,
+    message: 'You have exceeded the 100 requests in 24 hrs limit!', 
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+export const rateLimiterSlidingWindow = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000,
+    max: 100,
     message: 'You have exceeded the 100 requests in 24 hrs limit!', 
     standardHeaders: true,
     legacyHeaders: false,
