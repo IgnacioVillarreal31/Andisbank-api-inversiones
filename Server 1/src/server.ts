d8859cec4e72d603e96caac28e5cbee42000dab8
IgnacioVillarreal31 <ignacio.villarreal@correo.ucu.edu.uy>
2024-09-25 19:28:35 -0300

    update

--- Server 1/src/server.ts
+++ Server 1/src/server.ts
@@ -1,12 +1,14 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import investmentRoutes from './routes/investmentRoutes';
+//export { rateLimiter } from './middlewares/rateLimiter';
 
 const app: Application = express();
 
 app.use(bodyParser.json());
 
 app.use('/investments', investmentRoutes);
+//app.use(rateLimiter);
 
 const PORT = process.env.PORT || 3000;
 
