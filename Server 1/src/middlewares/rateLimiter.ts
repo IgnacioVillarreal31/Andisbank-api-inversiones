4cb778123cd06ddfae3adb7d13053c709f9eff6b
IgnacioVillarreal31 <ignacio.villarreal@correo.ucu.edu.uy>
2024-09-25 19:35:24 -0300

    a

--- Server 1/src/middlewares/rateLimiter.ts
+++ Server 1/src/middlewares/rateLimiter.ts
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
-export const rateLimiterUsingThirdParty = rateLimit({
+export const rateLimiterFixedWindow = rateLimit({
     windowMs: 24 * 60 * 60 * 1000,
-    max: 100,
+    max: 2,
     message: 'You have exceeded the 100 requests in 24 hrs limit!', 
     standardHeaders: true,
     legacyHeaders: false,
--- Server 1/src/routes/investmentRoutes.ts
+++ Server 1/src/routes/investmentRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getInvestments, getInvestment, postInvestmentAdmin, postBuyInvestmentUser, postSellInvestmentUser, deleteInvestmentAdmin, putInvestmentAdmin, getUserInvestment} from '../controllers/investmentController';
+import { rateLimiterFixedWindow } from '../middlewares/rateLimiter';
 
 const router: Router = Router();
 
-router.get('/getInvestments', getInvestments);
-router.get('/getInvestment/:idInvestment', getInvestment);
-router.post('/postInvestmentAdmin', postInvestmentAdmin);
+router.get('/getInvestments', rateLimiterFixedWindow, getInvestments);
+router.get('/getInvestment/:idInvestment', rateLimiterFixedWindow, getInvestment);
+router.post('/postInvestmentAdmin', rateLimiterFixedWindow, postInvestmentAdmin);
 router.post('/postBuyInvestmentUser', postBuyInvestmentUser);
 router.post('/postSellInvestmentUser', postSellInvestmentUser);
 router.delete('/deleteInvestmentAdmin/:idInvestment', deleteInvestmentAdmin);
